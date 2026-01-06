7f5f1113e44ecafb95f0ee45b0f3dae7a92d2791
goldsara22-byte <goldsara22@gmail.com>
2026-01-06 14:14:39 +0200

    Update postAPI.js

--- src/API/postAPI.js
+++ src/API/postAPI.js
@@ -7,5 +7,25 @@ async function getPostsByUser(user) {
     return mine;
 }
 
+async function deletePostById(id) {
+    const res = await deleteGeneralAPI(`/posts/${id}`);
+    if (!res.ok) throw new Error("delete failed");
+}
+
+async function patchPostComplite(post) {
+    const res = await patchGeneralAPI(`/posts/${post.id}`, { completed: !post.completed });
+    if (!res.ok) throw new Error("patch failed");
+    const updated = await res.json();
+    return updated;
+}
+
+
+async function patchPostTitleById(id, newTitle) {
+    const res = await patchGeneralAPI(`/posts/${id}`, { title: newTitle });
+    if (!res.ok) throw new Error("patch failed");
+    const updated = await res.json();
+    return updated;
+}
 export {getPostsByUser,deletePostById, patchPostComplite, patchPostTitleById}
+
 
