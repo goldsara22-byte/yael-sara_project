460b5a20179bfc168447de76c3b4e4abac5bb392
goldsara22-byte <goldsara22@gmail.com>
2026-01-06 14:12:23 +0200

    Add edit and delete functionality to SingleTodo

--- src/components/posts/singlePost.jsx
+++ src/components/posts/singlePost.jsx
@@ -1,1 +1,64 @@
+import EditButton from "../shared/EditButton.jsx";
+import DeleteButton from "../shared/DeleteButton.jsx";
+import { deletePostById, patchPostComplite, patchPostTitleById } from "../../API/postAPI.js";
 
+export default function SingleTodo({ todo,setTodos, onError }) {
+    async function handleDeletetodo(t) {
+    try {
+      await deleteTodoById(t.id);
+      setTodos(prev => prev.filter(td => String(td.id) !== String(t.id)));
+    } catch {
+      setErr("שגיאה במחיקת todo");
+      return;
+    }
+  }
+
+  async function toggleCompleted(todo) {
+    try {
+      const updated = await patchTodoComplite(todo);
+      setTodos((prev) =>
+        prev.map((t) => (String(t.id) === String(todo.id) ? updated : t))
+      );
+    } catch {
+      setErr("שגיאה בעדכון סטטוס todo");
+      return;
+    }
+  }
+  
+  async function updateTodoTitle(id, newTitle) {
+    try {
+      const updated = await patchTodoTitleById(id, newTitle);
+      setTodos((prev) =>
+        prev.map((t) => (String(t.id) === String(id) ? updated : t))
+      );
+    } catch {
+      setErr("שגיאה בעדכון כותרת todo");
+      return;
+    }
+  }
+
+  return (
+    <div className="todo-item">
+      <div className="todo-id">#{todo.id}</div>
+      <EditButton
+        todoId={todo.id}
+        title={todo.title}
+        onSave={updateTodoTitle}
+      />
+      <DeleteButton
+        onDelete={() => handleDeletetodo(todo)}
+        onError={() => onError("שגיאה במחיקת todo")}
+      >
+        ❌
+      </DeleteButton>
+      <label className="todo-done">
+        done
+        <input
+          type="checkbox"
+          checked={!!todo.completed}
+          onChange={() => toggleCompleted(todo)}
+        />
+      </label>
+    </div>
+  );
+}
